3c04f7104e7c011b90be57f2926fc45f3a05749c
LYZ <985951964@qq.com>
2022-05-25 22:29:10 +0800

    新增第7题与第8题代码

---
+++ cmake-build-debug-raspberry/CMakeFiles/wangdao_exercise.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/wangdao_exercise.dir/chapter_2/2.2.3_8.cpp.o"
+  "CMakeFiles/wangdao_exercise.dir/chapter_2/2.2.3_8.cpp.o.d"
+  "wangdao_exercise"
+  "wangdao_exercise.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/wangdao_exercise.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug-raspberry/CMakeFiles/wangdao_exercise.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for wangdao_exercise.
+# This may be replaced when dependencies are built.
